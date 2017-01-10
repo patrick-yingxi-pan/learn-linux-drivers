c573fed25dc6f40b7c2c226e9f7bf26d91c5dddb
Patrick Pan <pyxchina92929@gmail.com>
2017-01-10 01:56:13 -0800

    add cdev_add() in char/main.c

--- char/main.c
+++ char/main.c
@@ -1,29 +1,45 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/fs.h>
+#include <linux/cdev.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 
-static dev_t dev;
 static const char *device_name = "char";
 static unsigned int nr_devices = 4;
 
+static dev_t dev;
+static struct cdev cdev;
+
+static struct file_operations file_ops = {
+	
+};
+
+
 static int hello_init(void)
 {
-	int r = alloc_chrdev_region(&dev, 0, nr_devices, device_name);
-	if (r != 0) {
+	int err = alloc_chrdev_region(&dev, 0, nr_devices, device_name);
+	if (err) {
 		printk(KERN_ALERT "Cannot allocate dev region\n");
 		return -1;
-	} else {
-		printk(KERN_ALERT "Dev region allocated\n");
 	}
-	printk(KERN_ALERT "Hello, world\n");
+
+	cdev_init(&cdev, &file_ops);
+	cdev.owner = THIS_MODULE;
+
+	err = cdev_add(&cdev, dev, nr_devices);
+	if (err) {
+		printk(KERN_ALERT "cdev_add() failed\n");
+		return -1;
+	}
+		
 	return 0;
 }
 
 static void hello_exit(void)
 {
 	unregister_chrdev_region(dev, nr_devices);
+	cdev_del(&cdev);
 	printk(KERN_ALERT "Goodbye, cruel world\n");
 }
 
