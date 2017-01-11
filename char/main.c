720a6311a2893dff91054182d6b72aef8d38d493
Patrick Pan <pyxchina92929@gmail.com>
2017-01-11 01:04:00 -0800

    char/main.c: add char_read()

--- char/main.c
+++ char/main.c
@@ -2,21 +2,64 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
+#include <linux/slab.h>
+#include <asm/uaccess.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 
+static dev_t dev;
+static struct cdev cdev;
 static const char *device_name = "char";
 static unsigned int nr_devices = 4;
 
-static dev_t dev;
-static struct cdev cdev;
+int char_open(struct inode *inode, struct file *filp)
+{
+	printk("opened with minor number: %d\n", iminor(inode));
+	filp->private_data = (void *)(long)(iminor(inode) + '0');
+	return 0;
+}
+
+int char_release(struct inode *inode, struct file *filp)
+{
+	return 0;
+}
+
+ssize_t char_read(struct file *filp, char __user *buff, size_t count, loff_t *offp)
+{
+	char *p;
+	int err;
+	ssize_t retval = 0;
+
+	printk("char_read(%zu)\n", count);
+	p = kmalloc(count, GFP_KERNEL);
+	if (!p) {
+		printk("read() error: no memory left\n");
+		return -ENOMEM;
+	}
+
+	memset(p, (long)filp->private_data, count);
+	err = copy_to_user(buff, p, count);
+	if (err)
+		retval = -EFAULT;
+
+	kfree(p);
+	return count;
+}
+
+ssize_t char_write(struct file *filp, const char __user *buff,
+	size_t count, loff_t *offp)
+{
+	return 0;
+}
 
 static struct file_operations file_ops = {
-	
+	.open = char_open,
+	.release = char_release,
+	.read = char_read
 };
 
 
-static int hello_init(void)
+static int char_init(void)
 {
 	int err = alloc_chrdev_region(&dev, 0, nr_devices, device_name);
 	if (err) {
@@ -36,12 +79,12 @@ static int hello_init(void)
 	return 0;
 }
 
-static void hello_exit(void)
+static void char_exit(void)
 {
 	unregister_chrdev_region(dev, nr_devices);
 	cdev_del(&cdev);
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "%s.ko removed\n", device_name);
 }
 
-module_init(hello_init);
-module_exit(hello_exit);
+module_init(char_init);
+module_exit(char_exit);
